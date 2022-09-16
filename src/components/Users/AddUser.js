fbefcc0b8b244b3679a95b275b315fc066451afc
Syre <chestersyron.damian14@outlook.com>
2022-09-17 02:15:53 +0800

    Adding Validation and Reset Logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,16 +6,30 @@ import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUserName] = useState("");
+  const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    // Validation check if username and age is empty
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    // Validation check if age is less than 1 ... the + is used to force convert age to number
+    if (+enteredAge < 1) {
+      return;
+    }
+
     console.log(enteredUsername, enteredAge);
+
+    // Reset the current state by returning an empty value
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   const usernameChangeHandler = (event) => {
-    setEnteredUserName(event.target.value);
+    setEnteredUsername(event.target.value);
   };
 
   const ageChangeHandler = (event) => {
@@ -26,9 +40,19 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
-        <label htmlFor="age">Age</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submut">Add User</Button>
       </form>
     </Card>
